b4fcbc4c8659b5f9f0ec8b6d5805591de378deaa
Bob <>
2023-07-21 18:46:48 +0800

    cli 2

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,9 @@
+use std::error::Error;
+
+use std::fs;
+
+use std::env;
+
 pub fn add_two(a: i32) -> i32 {
     internal_adder(a, 2)
 }
@@ -15,5 +21,106 @@ mod tests {
     #[test]
     fn internal() {
         assert_eq!(4, internal_adder(2, 2))
+    }
+}
+
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.filename)?;
+
+    let results = if config.case_sensitive {
+        search(&config.query, &contents)
+    } else {
+        search_case_insensentive(&config.query, &contents)
+    };
+
+    for line in results {
+        println!("{}", line);
+    }
+
+    Ok(())
+}
+
+pub struct Config {
+    pub query: String,
+    pub filename: String,
+    pub case_sensitive: bool,
+}
+
+impl Config {
+    pub fn new(args: &Vec<String>) -> Result<Config, &str> {
+        if args.len() < 3 {
+            return Err("not enough args");
+        }
+
+        let query = args[1].clone();
+        let filename = args[2].clone();
+
+        let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
+
+        Ok(Self {
+            query,
+            filename,
+            case_sensitive,
+        })
+    }
+}
+
+fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut result = vec![];
+
+    for line in contents.lines() {
+        if line.contains(query) {
+            result.push(line);
+        }
+    }
+
+    result
+}
+
+fn search_case_insensentive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let query = query.to_lowercase();
+
+    let mut result: Vec<&str> = vec![];
+
+    for line in contents.lines() {
+        if line.to_lowercase().contains(&query) {
+            result.push(line);
+        }
+    }
+
+    result
+}
+
+#[cfg(test)]
+mod tests_cli {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+
+        let contents = "
+Rust: 
+safe, fast, productive.
+pic three;
+		";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents))
+    }
+
+    #[test]
+    fn case_insensentive() {
+        let query = "rust";
+
+        let contents = "
+Rust:
+safe, fast, productive.
+pic three.
+Trust me.";
+
+        assert_eq!(
+            vec!["Rust:", "Trust me."],
+            search_case_insensentive(query, contents)
+        )
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -3,55 +3,29 @@
 use colored::Colorize;
 use rand::Rng;
 use std::cmp::Ordering;
-use std::error::Error;
 use std::io;
 
 use std::env;
-use std::fs;
 
 use std::process;
+
+use hello_guessing_game::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
     let config = Config::new(&args).unwrap_or_else(|err| {
-        print!("{}", err);
+        eprint!("{}", err);
         process::exit(1);
     });
 
     println!("{}", config.query);
     println!("{}", config.filename);
 
-    if let Err(err) = run(config) {
-        println!("{}", err);
+    if let Err(err) = hello_guessing_game::run(config) {
+        eprintln!("{}", err);
         process::exit(1);
     };
-}
-
-fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let contents = fs::read_to_string(config.filename)?;
-
-    println!("{}", contents);
-
-    Ok(())
-}
-
-struct Config {
-    query: String,
-    filename: String,
-}
-
-impl Config {
-    fn new(args: &Vec<String>) -> Result<Config, &str> {
-        if args.len() < 3 {
-            return Err("not enough args");
-        }
-
-        let query = args[1].clone();
-        let filename = args[2].clone();
-
-        Ok(Self { query, filename })
-    }
 }
 
 fn guessing_game() {
